8ccc218efac992dfd89737e4ca5a97826e05eeb1
VELGEIM <79646742+VELGEIM@users.noreply.github.com>
2026-04-17 23:35:05 +0200

    db.py

--- db.py
+++ db.py
@@ -4,23 +4,28 @@ import os
 pool = None
 
 
-async def init():
+async def init_db():
     global pool
     pool = await asyncpg.create_pool(os.getenv("DATABASE_URL"))
 
-    async with pool.acquire() as c:
-        await c.execute("""
+    async with pool.acquire() as conn:
+        await conn.execute("""
         CREATE TABLE IF NOT EXISTS users(
             user_id BIGINT PRIMARY KEY,
             username TEXT,
             first_name TEXT,
             expire BIGINT DEFAULT 0,
             last_pay BIGINT DEFAULT 0,
-            banned INT DEFAULT 0
+            is_banned INT DEFAULT 0
         )
         """)
 
 
-async def user(uid):
-    async with pool.acquire() as c:
-        return await c.fetchrow("SELECT * FROM users WHERE user_id=$1", uid)
+async def get_user(uid):
+    if not pool:
+        raise RuntimeError("DB pool not initialized")
+
+    async with pool.acquire() as conn:
+        return await conn.fetchrow(
+            "SELECT * FROM users WHERE user_id=$1", uid
+        )
