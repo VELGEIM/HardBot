b4b99bd4c1f2c180b907d4bb74ad59a925c9b557
VELGEIM <79646742+VELGEIM@users.noreply.github.com>
2026-04-19 14:55:00 +0200

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-TOKEN = os.getenv("BOT_TOKEN")
-DB_URL = os.getenv("DATABASE_URL")
-PUBLIC_URL = os.getenv("PUBLIC_URL")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID", "0"))
-PORT = int(os.getenv("PORT", "10000"))
-
-ADMIN_IDS = set(map(int, os.getenv("ADMIN_IDS", "").split(","))) if os.getenv("ADMIN_IDS") else set()
